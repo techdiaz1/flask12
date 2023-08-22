7965d145b161dbbdcf51be50fe3a9809ef35fb06
techdiaz1 <92034508+techdiaz1@users.noreply.github.com>
2023-08-22 12:08:10 +0530

    Update flask_hello.py

--- flask_hello.py
+++ flask_hello.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
-
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    app.run(debug=True)
+#app.run(host='0.0.0.0', port=81)
